80b95937f40d1ab6029fb48794b013a90eb525ad
gjotuur <v.chekhovskyi@proton.me>
2025-11-13 22:17:33 +0200

    Added ring multiplication and power for further algs

--- NTA_L01.c
+++ NTA_L01.c
@@ -3,24 +3,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <stdint.h>
+
+//Realisation: tests on primeness, integer factorisation algorithms, "smart" function to determine best algorithm
+//and find any prime
+
+/*Part 1: primeness tests 
+1.1 Solovey-Strassen test
+1.2 Miller-Rabine test
+*/
 
 int EEA(int a, int n, int* u, int* v);
-int reduce(int a, int n);
+bool SS_test(uint64_t p, int k); //two args: number & test precision
+
+static inline uint64_t ring_mul(uint64_t n1, uint64_t n2, uint64_t mod);
+static inline uint64_t ring_pow(uint64_t base, uint64_t exponent, uint64_t mod);
 
 int main(){
-    //Tests for EEA
-    int a = 0, n = 0;
-    int x, y;
-    printf("Enter the numbers to analyze");
-    printf("\na: ");
-    scanf("%d", &a);
-    printf("\nb: ");
-    scanf("%d", &n);
-    int g = EEA(a, n, &x, &y);
-    printf("\nGCD of %d and %d is %d", a, n, g);
-    printf("\nBezout`s equation: %d + %d = %d", a*x, n*y, g);
-
-
+    printf("\nLehho Dorlw!");
+    printf("\nTasks: \n1. Primeness tests \n2. Factorization algs");
+    printf("\nRing mul & Ring pow tests");
+    uint64_t a = ring_mul(11,11,7);
+    uint64_t b = ring_pow(a,a,7);
+    printf("\n11 * 11 (mod 7) = %llu \n %llu ^ %llu (mod 7) = %llu", a, a, a, b);
+    
     return 0;
 }
 
@@ -38,12 +44,23 @@ int EEA(int a, int n, int* u, int* v){
     return gcd;
 }
 
-int reduce(int a, int n){
-    if(a < n){
-        return a;
+bool SS_test(uint64_t p, int k){
+    if(p <= 2){
+        return true;
     }
-    while(a > n){
-        a -= n;
+    return 0;
+}
+
+static inline uint64_t ring_mul(uint64_t n1, uint64_t n2, uint64_t mod){
+    return (uint64_t)((__uint128_t) n1 * n2 % mod);
+}
+
+static inline uint64_t ring_pow(uint64_t base, uint64_t exponent, uint64_t mod){
+    uint64_t rsd = 1;
+    while(exponent){
+        if(exponent & 1) rsd = ring_mul(rsd, base, mod);
+        base = ring_mul(base, base, mod);
+        exponent >>= 1;
     }
-    return a;
+    return rsd;
 }
