9183d2681f404e850e69ef0e6ab6a9e6294df288
gjotuur <v.chekhovskyi@proton.me>
2025-11-15 02:40:57 +0200

    Primeness tests passed, message has a little bug with probability

--- NTA_L01.c
+++ NTA_L01.c
@@ -16,7 +16,9 @@
 
 uint64_t rnd_between(uint64_t x, uint64_t y);                           //simple RNG, to do it without srand() and rand
 uint64_t EEA(uint64_t a, uint64_t n, uint64_t* u, uint64_t* v);         //Extended Euclidean alg 
-bool SS_test(uint64_t p, int k);                                        //Solovey-Strassen test
+bool SS_test(uint64_t p, int k, uint64_t* counter);                     //Solovey-Strassen test
+void test_number(uint64_t number, int precision);                       //Test with consol message
+int8_t JCB(uint64_t a, uint64_t n);                                     //Jacobi symbol calculation
 
 static inline uint64_t ring_mul(uint64_t n1, uint64_t n2, uint64_t mod);            //actually, it`s mulmod, but i don`t like the name
 static inline uint64_t ring_pow(uint64_t base, uint64_t exponent, uint64_t mod);    //actually, it`s powmod, but i don`t like the name as well :D
@@ -42,6 +44,19 @@ int main(){
     for(int i = 0; i < 10; i++){
         printf("%2d. %6llu\n", i+1, rnd_list[i]);
     }
+    int j1 = JCB(1719, 78936);
+    int j2 = JCB(25799, 915133);
+    printf("\n(1719 / 78936) = %d \n(25799 / 915133) = %d", j1, j2);
+    uint64_t test1_num =  858135414131218189;
+    test_number(test1_num, 100);
+    uint64_t test2_num =  123456789012345671;
+    test_number(test2_num, 200);
+    uint64_t test3_num =  987654321098765429;
+    test_number(test3_num, 200);
+    test1_num = 123456789012345678;
+    test_number(test1_num, 100);
+    test2_num = 987654321098765433;
+    test_number(test2_num, 100);
 
     return 0;
 }
@@ -60,22 +75,53 @@ uint64_t EEA(uint64_t a, uint64_t n, uint64_t* u, uint64_t* v){
     return gcd;
 }
 
-bool SS_test(uint64_t p, int k){
-    uint64_t counter = 0;
-    bool conclusion = false;
-    if(p <= 2){
+bool SS_test(uint64_t p, int k, uint64_t* counter){
+    int c = 0;
+    if (p == 2) {
+        if(counter) *counter = k;  // всі k ітерацій "пройшли"
         return true;
     }
-    uint64_t seed = rnd_between(1, p);
-    uint64_t u,v;
-    uint64_t n1 = EEA(seed, p, &u, &v);
-    if(n1 != 1){
+    if((p%2) == 0 || p <2) {
+        if(counter) *counter = 0;  // не виконано жодної ітерації
         return false;
     }
-    //step 2: Eulers pseudoprime by base (sry, PC battery is almost empty...)
+    while(c < k){
+        uint64_t seed = rnd_between(2, p - 1);
+        uint64_t u,v;
+        uint64_t n1 = EEA(seed, p, &u, &v);
+        if(n1 != 1) {
+            if(counter) *counter = c;
+            return false;
+        }
+        uint64_t condition_a = ring_pow(seed, ((p - 1) / 2), p);
+        int64_t condition_b = (int64_t)JCB(seed, p);
 
+        //Normalize
+        int64_t normalized_a;
+        if (condition_a == p - 1) {
+            normalized_a = -1;
+        } else {
+            normalized_a = (int64_t)condition_a;
+        }
+        if(normalized_a != condition_b) {
+            if(counter) *counter = c;
+            return false;
+        }
+        c++;
+    }
+    if(counter) *counter = (uint64_t)c;
+    return true;
+}
 
-    return 0;
+void test_number(uint64_t number, int precision){
+    uint64_t k;
+    bool conclusion = SS_test(number, precision, &k);
+    double probability = 1 - pow(2, (double)(-k));
+    if(conclusion){
+        printf("\nNumber %20llu is prime with probability %2.2lf", number, probability*100);
+    } else {
+        printf("\nThe number %20llu is not prime", number);
+    }
 }
 
 static inline uint64_t ring_mul(uint64_t n1, uint64_t n2, uint64_t mod){ //max 64 bit, less stress
@@ -92,11 +138,36 @@ static inline uint64_t ring_pow(uint64_t base, uint64_t exponent, uint64_t mod){
     return rsd;
 }
 
+//simple RNG (built-in srand || rand sucks)
 uint64_t rnd_between(uint64_t x, uint64_t y){
     uint64_t range = y - x + 1;
     uint64_t r, lim = UINT64_MAX - (UINT64_MAX % range);
     do {
-        r = ((uint64_t)rand() << 32) ^ rand();
-    } while (x >= lim);
+        r = ((uint64_t)rand() << 32) ^ (uint64_t)rand();
+    } while (r >= lim);
     return x + r % range;
+}
+
+//Jacobi sign (symbol?) calculation for SS test
+int8_t JCB(uint64_t a, uint64_t n){
+    int64_t jcb = 1;
+    if(n <= 0 || (n % 2) == 0) return 0;    
+    if(a == 1) return 1;                    
+    a %= n;                                 
+    while(a != 0){
+        while((a % 2) == 0){
+            a /= 2;
+            if(n % 8 == 3 || n % 8 == 5){
+                jcb = -jcb;
+            }
+        }
+        uint64_t tmp = a;
+        a = n;
+        n = tmp;
+        if(a % 4 == 3 && n % 4 == 3){
+            jcb = -jcb;
+        }
+        a %= n;
+    }
+    return (n == 1) ? (int8_t)jcb : 0;
 }
